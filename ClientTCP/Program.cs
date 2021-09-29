91dfcb912aaacb37bc42594aad629193e3e0342a
emaksim <eg92saratov@mail.ru>
2021-09-29 14:22:28 +0400

    test socket app

---
+++ ClientTCP/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ClientTCP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string ip = "127.0.0.1";
+            const int port = 8080;
+
+            while (true)
+            {
+                var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+                var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                var message = Console.ReadLine();
+                Console.WriteLine("Пользователь 1: " + message);
+                var data = Encoding.UTF8.GetBytes(message);
+
+                tcpSocket.Connect(tcpEndPoint);
+                tcpSocket.Send(data);
+
+                var buffer = new byte[256];
+                var size = 0;
+                var answer = new StringBuilder();
+
+                do
+                {
+                    size = tcpSocket.Receive(buffer);
+                    answer.Append(Encoding.UTF8.GetString(buffer, 0, size));
+                } while (tcpSocket.Available > 0);
+
+                tcpSocket.Shutdown(SocketShutdown.Both);
+                tcpSocket.Close();
+            }
+        }
+    }
+}
